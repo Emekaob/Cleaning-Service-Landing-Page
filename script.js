a167fecfc8474faacc54f14f6639abd19f21b8cd
Emeka <onyeakaemeka42@gmail.com>
2026-06-17 20:16:30 +0100

    Added the list of services

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const navBar = document.getElementById("nav-content");
 hamburger.addEventListener("click", () => {
   navBar.classList.toggle("active");
 });
+
+const links = document.querySelectorAll(".nav-bar a");
+const currentPage = window.location.pathname.split("/").pop();
+
+links.forEach((link) => {
+  if (link.getAttribute("href") === currentPage) {
+    link.classList.add("active");
+  }
+});
