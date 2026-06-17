7d19e96268e1b6fbc572a050ac9c1e6b1e208c6f
Emeka <onyeakaemeka42@gmail.com>
2026-06-17 22:16:51 +0100

    Updated the service page

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ links.forEach((link) => {
     link.classList.add("active");
   }
 });
+
+const images = document.querySelectorAll("img");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+
+images.forEach((image) => observer.observe(image));
